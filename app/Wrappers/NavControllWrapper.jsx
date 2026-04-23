65cae7e73da925748b0841bf71f1bc28745a6b46
prajwal003-web-developer <prajwalneupane003@gmail.com>
2026-04-23 21:26:35 +0545

    first commit

--- app/Wrappers/NavControllWrapper.jsx
+++ app/Wrappers/NavControllWrapper.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../Components/Navbars/SideBar'
 import TopBar from '../Components/Navbars/TopBar'
 import useNavControl from '../Stores/UseNavControl'
@@ -8,18 +8,31 @@ import { LoaderIcon } from 'lucide-react'
 import useKundaliStores from '../Stores/KundaliStores'
 
 const NavControllWrapper = (  {children}) => {
+
+  const [Loading, setLoading] = useState(true)
     
     const {isSidebarOpen} = useNavControl()
 
      const {setKundaliDatas} = useKundaliStores()
 
      useEffect(()=>{
+
+      setTimeout(()=>{
+        setLoading(false)
+      },1000)
       const dataString = localStorage.getItem("Kundalis")
       const data = dataString? JSON.parse(dataString):[]
 
       setKundaliDatas(data)
      },[])
 
+
+
+    if(Loading){
+    return  <div className='flex justify-center items-center h-[100dvh]'>
+        <LoaderIcon className='duratio-700 animate-spin' size={64}/>
+      </div>
+  }
 
   return (
     <div>
